45f5220cfcceacd915086bc520277f500c691969
ASHUTOSH RAJ <ashutoshraj0420@gmail.com>
2022-04-01 02:51:07 +0530

    Adding data

---
+++ AshutoshJSFile/signIn.js
@@ -0,0 +1,26 @@
+
+// for login giving the functionallity
+
+var userData = JSON.parse(localStorage.getItem("userDatabase"));
+     let form = document.querySelector(".form");
+     form.addEventListener("submit",addDetails);
+
+  function addDetails(event){
+      event.preventDefault()
+
+        let email = form.user.value;
+        let password = form.pass.value;
+
+        
+        for(var i=0; i<userData.length; i++){
+            if((email == userData[i].email) && (password == userData[i].password)){
+                 alert("login successfull");
+                // window.location.href = "signup.html";
+                break;
+            }
+            else{
+                alert("Invalid Email or Password");
+                break;
+            }
+        }
+  }
